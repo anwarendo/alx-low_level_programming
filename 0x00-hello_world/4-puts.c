a62687ec35830143cdd9041c9d6470f9b57adbac
Anwar Endris <anwarendo@gmail.com>
2022-06-30 04:20:08 -0700

    Fix again

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,10 @@
 
 /**
  * main - The main function
+ * Return: 0 for success
  */
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-        return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
 }
