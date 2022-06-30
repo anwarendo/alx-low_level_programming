b12b64be480a38f834a5e8c82d8f5c6f4932780c
Anwar Endris <anwarendo@gmail.com>
2022-06-30 04:57:39 -0700

    Fix for coding style

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,6 +9,7 @@ int main(void)
 {
 	char c;
 	long long n;
+	
 	printf("Size of a char: %ld byte(s)\n", sizeof(c));
 	printf("Size of an int: %ld byte(s)\n", sizeof(int));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(long));
