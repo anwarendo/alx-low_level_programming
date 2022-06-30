371c58f386d886e11d79cd31c8b51fa4d245d598
Anwar Endris <anwarendo@gmail.com>
2022-06-30 04:08:39 -0700

    Fix again

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
 
-/* Prints the text as it is */
+/**
+ * main - The main function
+ */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle\n");
-  return 0;
+        puts("\"Programming is like building a multilingual puzzle");
+        return 0;
 }
