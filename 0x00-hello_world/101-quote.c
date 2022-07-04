ff28247c98ba2c7f408916501efff24a3ebe7412
Anwar Endris <anwarendo@gmail.com>
2022-07-04 00:08:05 -0700

    Fix task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-	char c[60] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n";
+	char c[60] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	int i = 0;
 
-	while(i < 60)
+	while (i < 60)
 	{
 		putchar(c[i++]);
 	}
