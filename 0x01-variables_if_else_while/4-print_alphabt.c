4b1ad92f3cad533cebe43a8e7541e8aa93a07eca
Anwar Endris <anwarendo@gmail.com>
2022-07-04 07:42:18 -0700

    Add task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - The entry
+ * Return: 0 for success
+ */
+int main(void)
+{
+	char c;
+	char nl;
+
+	c = 'a';
+	nl = '\n';
+	while (c <= 'z')
+	{
+		if (c == 'e' || c == 'q')
+			continue;
+		putchar(c++);
+	}
+	putchar(nl);
+	return (0);
+}
