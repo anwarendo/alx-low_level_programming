54f188c1e197f5648f6e36f961f1d664b30d2561
Anwar Endris <anwarendo@gmail.com>
2022-07-05 01:44:42 -0700

    Add task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - The entry function
+ * Return: 0 for success
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+	C source files ==> what they do?
+	* 0-putchar.c  ==> prints "_putchar"
+	
