c5854f3362327158cb4272535d706f1afdedb6f2
Anwar Endris <anwarendo@gmail.com>
2022-07-05 05:35:05 -0700

    Fix typo for Betty of task 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,7 +3,7 @@
 
 /**
  * _abs - computes the absolute value of the integer argument
- * @: the integer argument
+ * @n: the integer argument
  * Return: the absolute value of the integer argument
  */
 int _abs(int n)
