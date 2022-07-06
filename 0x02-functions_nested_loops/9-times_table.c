ccb06ba79384d6e66aa50777f5d3c9732b500527
Anwar Endris <anwarendo@gmail.com>
2022-07-06 04:33:57 -0700

    Fix task 9 ..

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,7 +16,9 @@ void times_table(void)
 		for (j = 0; j < 10; j++)
 		{
 			p = i * j;
-			if (j < 9)
+			if (j == 0)
+			        printf("%d,", p);
+			else if (j > 0 && j < 9)
 				printf("%3d,", p);
 			else
 				printf("%3d\n", p);
