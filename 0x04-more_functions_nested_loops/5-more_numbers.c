79158cf07e8b9b9c8283ac6b97b23cd01f19cad2
Anwar Endris <anwarendo@gmail.com>
2022-07-08 23:28:05 +0300

    Add task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,12 +8,19 @@ void more_numbers(void)
 {
 	int i;
 	int j;
+	int j_char_left;
+	int j_char_right;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 48; j < 59; j++)
+		for (j = 0; j < 15; j++)
 		{
-			_putchar(j);
+			j_char_left = j / 10;
+			j_char_right = j % 10;
+
+			if (j >= 10)
+				_putchar(j_char_left + 48);
+			_putchar(j_char_right + 48);
 		}
 		_putchar('\n');
 	}
