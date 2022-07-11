7e660f28f61468556089c3ada15b8e5bd0d8b51b
Anwar Endris <anwarendo@gmail.com>
2022-07-11 22:41:24 +0300

    Fix task 8 ...

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,8 +10,10 @@
 void print_array(int *a, int n)
 {
 	int i;
-
-	for (i = 0; i < n - 1; i++)
-		printf("%d, ", a[i]);
+	if (n > 0)
+	{
+		for (i = 0; i < n - 1; i++)
+			printf("%d, ", a[i]);
+	}
 	printf("%d\n", a[i]);
 }
