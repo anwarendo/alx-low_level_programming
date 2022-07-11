f522be5b18346aea3a44bdbb5402e3682d1b71c1
Anwar Endris <anwarendo@gmail.com>
2022-07-11 15:13:30 +0300

    Add task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - resets the arguments value of the pointer to 98
+ * @*c: pointer to integer
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
