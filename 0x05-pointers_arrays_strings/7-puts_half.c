e03a37a9ebbf5e9bf7a4a534bf777fae9045d63d
Anwar Endris <anwarendo@gmail.com>
2022-07-11 22:03:13 +0300

    Fix task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,9 +12,9 @@ void puts_half(char *str)
 	int i;
 	int half;
 	if (len % 2 != 0)
-		half = (len - 1)/2;
+		half = (len - 1) / 2;
 	else
-		half = len/2;
+		half = len / 2;
 	for (i = half; i < len; i++)
 		_putchar(str[i]);
 	_putchar('\n');
