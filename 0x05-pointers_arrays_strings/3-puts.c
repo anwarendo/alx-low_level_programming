8b75806dd45ff7c25763cc31bebdf140b2f36135
Anwar Endris <anwarendo@gmail.com>
2022-07-11 15:49:37 +0300

    Add task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _puts - prints string to stdout
+ * @str - pointer to the beginning of the string to be printed out
+ * Return: void
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+		_putchar(*str++);
+}
