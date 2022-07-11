2018a14606670ad80ce339b4ab2aec971e3594b1
Anwar Endris <anwarendo@gmail.com>
2022-07-11 21:43:47 +0300

    Add task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half - prints half of a string
+ * @str: the pointer argument for the string
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int len = strlen(str);
+	int i;
+	int half;
+	if (len % 2 != 0)
+		half = (len - 1)/2;
+	else
+		half = len/2;
+	for (i = half; i < len; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
