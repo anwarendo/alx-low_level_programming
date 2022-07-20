da1795df017163e72aea3efe1f8ca1131324e959
Anwar Endris <anwarendo@gmail.com>
2022-07-20 19:43:10 +0300

    Add task 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -8,6 +8,9 @@
  */
 int _pow_recursion(int x, int y)
 {
+	long z;
+
+	z = 0;
 	if (y < 0)
 		return (-1);
 	else if ( y == 0)
@@ -15,6 +18,7 @@ int _pow_recursion(int x, int y)
 	else
 	{
 		y--;
-		return (x * _pow_recursion(x, y));
+		z = x * _pow_recursion(x, y);
+		return (z);
 	}
 }
