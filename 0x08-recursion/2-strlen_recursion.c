e4c82786b156d8faefc4966ed7d69c63a612a215
Anwar Endris <anwarendo@gmail.com>
2022-07-20 17:34:04 +0300

    Add task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - computes the length of a string
+ * @s: pointer for the string
+ * Return: the length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	return (sizeof(s));
+}
