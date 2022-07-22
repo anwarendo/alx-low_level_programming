c5113e31e4f8bd712c6e6ff883a6debabb0b69c4
Anwar Endris <anwarendo@gmail.com>
2022-07-22 18:01:32 +0300

    Add task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - the main function that prints the number of arguments
+ * @argc: number of arguments
+ * @argv: the argument vector or array of pointers for argument
+ * Return: 0 for success
+ */
+int main(int argc, char **argv)
+{
+	if(sizeof(argv))
+		printf("%d\n", argc);
+	return (0);
+}
