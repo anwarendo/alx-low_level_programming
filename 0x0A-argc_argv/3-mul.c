2ce316f4d0da5da315dbc0cd49c042325bc7982e
Anwar Endris <anwarendo@gmail.com>
2022-07-22 19:35:18 +0300

    Fix task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,14 +11,16 @@ int main(int argc, char *argv[])
 {
 	int x;
 	int y;
+	int z;
 
 	x = atoi(argv[1]);
 	y = atoi(argv[2]);
+	z = x * y;
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	printf("%d\n", x * y);
+	printf("%d\n", z);
 	return (0);
 }
