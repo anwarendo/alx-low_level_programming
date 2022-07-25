fabda6648dc2ce82e3b118a5bd78c37eceba4118
Anwar Endris <anwarendo@gmail.com>
2022-07-26 00:50:46 +0300

    Fix task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -20,9 +20,9 @@ char *str_concat(char *s1, char *s2)
 	if (s1 == NULL || s2 == NULL)
 	{
 		if (s1 == NULL)
-			s1[0] = '';
+			s1[0] = "";
 		if (s2 == NULL)
-			s2[0] = '';
+			s2[0] = "";
 	}
 
 	size1 = strlen(s1) * sizeof(char);
