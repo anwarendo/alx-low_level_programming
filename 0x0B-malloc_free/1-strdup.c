226790ac134336d6bc987979287836ddedfb7016
Anwar Endris <anwarendo@gmail.com>
2022-07-25 18:51:05 +0300

    Add task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - computes a newly allocated space
+ * @str: the string for which memory is to be allocated
+ * Return: pointer to the newly allocated space which contains copy of z string
+ */
+char *_strdup(char *str)
+{
+	char *ptr;
+	unsigned int size;
+	unsigned int i;
+
+	if (str == NULL)
+	{
+		ptr = NULL;
+		return (ptr);
+	}
+	size = sizeof(str);
+	ptr = malloc(size);
+	if (ptr)
+	{
+		for (i = 0; i < size; i++)
+			ptr[i] = str[i];
+		return (ptr);
+	}
+	else
+	{
+		ptr = NULL;
+		return (ptr);
+	}
+}
