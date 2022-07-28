a88d03ca645c162f100956769f4736da93a90140
Anwar Endris <anwarendo@gmail.com>
2022-07-28 11:55:06 +0300

    Add task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory
+ * @b: bytes size for allocation
+ * Return: nothing
+ */
+void *malloc_checked(unsigned int b)
+{
+	char *ptr;
+
+	ptr = malloc(b);
+	if (ptr)
+		return (ptr);
+	else
+		exit (98);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,8 @@
+	task files	==>	required to
+	0-malloc_checked.c	==>	 allocate memory using malloc
+	1-string_nconcat.c	==>	 concatenate two strings
+	2-calloc.c		==>	 allocate memory for an array
+	3-array_range.c		==>	 create an array of integers
+	100-??
+	101-??
+	
