bbfe55f98b2a0490db5ce65d4cdd9af44fd134b4
Anwar Endris <anwarendo@gmail.com>
2022-08-08 11:07:41 +0300

    Update task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stddef.h>
 
 /**
  * print_name - prints a name
@@ -8,5 +9,6 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (f != NULL && name != NULL)
+		f(name);
 }
