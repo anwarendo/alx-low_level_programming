0c63c34e449b028efadd8975cc2c62896c805b0a
Anwar Endris <anwarendo@gmail.com>
2022-08-08 14:44:24 +0300

    Update task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,6 +14,6 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (array != NULL && size != 0 && cmp != NULL)
 		for (i = 0; i < size; i++)
 			if (cmp(array[i]))
-			    return (i);
+				return (i);
 	return (-1);
 }
