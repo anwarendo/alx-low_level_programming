dc1d8569bc47b0b759b43fd638a97ff4eeefe65a
Anwar Endris <anwarendo@gmail.com>
2022-08-12 02:48:06 +0300

    Add task 2

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,10 +3,20 @@
 #include <stddef.h>
 #endif /* STDDEF */
 
+#ifndef STDLIB
+#define STDLIB
+#include <stdlib.h>
+#endif /* STDLIB */
+
 #ifndef STDIO
 #define STDIO
 #include <stdio.h>
 #endif /* STDIO */
+
+#ifndef STRING
+#define STRING
+#include <string.h>
+#endif /* STRING */
 
 #ifndef LIST_S
 #define LIST_S
@@ -32,3 +42,8 @@ size_t print_list(const list_t *h);
 #define LIST_LEN
 size_t list_len(const list_t *h);
 #endif /* LIST_LEN */
+
+#ifndef ADD_NODE
+#define ADD_NODE
+list_t *add_node(list_t **head, const char *str);
+#endif /* ADD_NODE */
