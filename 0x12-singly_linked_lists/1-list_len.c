3833a7082a30dc9a4cf0527e2e5eba93f2a8f53e
Anwar Endris <anwarendo@gmail.com>
2022-08-12 01:51:31 +0300

    Update task 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -11,16 +11,13 @@ size_t count_nodes(const list_t *node)
 		return (0);
 	if (node->next == NULL)
 		return (1);
-	else
-	{
-		return (1 + count_nodes(node->next));
-	}
+	return (1 + count_nodes(node->next));
 }
 
 /**
  * list_len - prints all the elements of a list
  * @h: pointer for a list_t structure
- * Return: the number of nodes (of type list_s structure)
+ * Return: the number of nodes of type list_s structure
  */
 size_t list_len(const list_t *h)
 {
