b1bca0390a38d48c05b70db2171b85a68ed0c959
Anwar Endris <anwarendo@gmail.com>
2022-08-12 03:13:32 +0300

    Update task 4

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,6 +1,19 @@
 #include "lists.h"
 
 /**
+ * free_node - frees a single node
+ * @node: the node to be freed
+ * Return: nothing
+ */
+void free_node(list_t *node)
+{
+	if (node->next == NULL)
+		free(node);
+	else
+		free_node(node->next);
+}
+
+/**
  * free_list - frees a list_t list
  * @head: the head of the list
  * Return: nothing
