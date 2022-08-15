2ab487dfd35be16bf95b1c21454af065d2abf467
Anwar Endris <anwarendo@gmail.com>
2022-08-15 11:46:24 +0300

    Add task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * count_nodes - counts nodes recursively
+ * @node: a node from the list
+ * Return: the count
+ */
+size_t count_nodes(const listint_t *node)
+{
+	if (node == NULL)
+		return (0);
+	if (node->next == NULL)
+		return (1);
+	printf("%d\n%d\n", node->n, node->next->n);
+	return (1 + count_nodes(node->next));
+}
+
+/**
+ * print_list - prints all the elements of a list
+ * @h: pointer for a list_t structure
+ * Return: the number of nodes of type list_s structure
+ */
+size_t print_listint(const listint_t *h)
+{
+	return (count_nodes(h));
+}
---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,28 @@
+#ifndef STDIO
+#define STDIO
+#include <stdio.h>
+#endif /* STDIO */
+
+#ifndef LISTINT_S
+#define LISTINT_S
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ *
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+#endif /* LISTINT_S */
+
+#ifndef PRINT_LISTINT
+#define PRINT_LISTINT
+size_t print_listint(const listint_t *h);
+#endif /* PRINT_LISTINT */
