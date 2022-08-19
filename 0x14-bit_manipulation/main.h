aaaa64696090a2168c6d7d52e61e5f043a95fc26
Anwar Endris <anwarendo@gmail.com>
2022-08-19 10:29:08 +0300

    Update task 0

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -3,6 +3,11 @@
 #include <stdlib.h>
 #endif /* STDLIB */
 
+#ifndef _POW
+#define _POW
+unsigned int _pow(unsigned int n);
+#endif /* _POW */
+
 #ifndef BINARY_TO_UINT
 #define BINARY_TO_UINT
 unsigned int binary_to_uint(const char *b);
